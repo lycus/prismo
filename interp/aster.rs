a455a24cccf8bfb619ca84a6050262c15c09f347
Tony Young <tony@rfw.name>
2013-05-06 18:02:44 +1200

    Move gen_lit to types.

--- interp/aster.rs
+++ interp/aster.rs
@@ -163,7 +163,7 @@ fn unify_pattern_basic(interp: @mut Interp, env: @mut env::Env<Interp>, pat: &as
             types::Unit => true,
             _ => false
         },
-        ast::LiteralPattern(lit) => *val == gen_lit(lit),
+        ast::LiteralPattern(lit) => *val == types::gen_lit(lit),
         ast::SymbolPattern(sym) => {
             env::declare(env, &sym, val);
             true
@@ -230,99 +230,12 @@ fn unify_pattern_let(interp: @mut Interp, env: @mut env::Env<Interp>, pat: &ast:
     }
 }
 
-fn unescape_str(inp: &str) -> ~str {
-    let mut out = ~[];
-    let mut i = 1;
-    let mut slice = inp.slice(1, inp.len() - 1);
-
-    while slice.len() > 0 {
-        out += ~[if slice.char_at(0) == '\\' {
-            match slice.char_at(1) {
-                // \" -> "
-                '"' => {
-                    i += 1;
-                    '"'
-                },
-
-                // \' -> '
-                '\'' => {
-                    i += 1;
-                    '\''
-                },
-
-                // \\ -> \
-                '\\' => {
-                    i += 1;
-                    '\\'
-                },
-
-                // \n -> <LF>
-                'n' => {
-                    i += 1;
-                    '\n'
-                },
-
-                // \r -> <CR>
-                'r' => {
-                    i += 1;
-                    '\r'
-                },
-
-                // \t -> <TAB>
-                't' => {
-                    i += 1;
-                    '\t'
-                },
-
-                // \x## -> hex escape
-                'x' => {
-                    if slice.len() >= 4 {
-                        i += 3;
-                        int::from_str_radix(slice.slice(2, 4), 16).unwrap() as char
-                    } else {
-                        'x'
-                    }
-                },
-
-                // \u#### -> unicode escape
-                'u' => {
-                    if slice.len() >= 6 {
-                        i += 5;
-                        int::from_str_radix(slice.slice(2, 6), 16).unwrap() as char
-                    } else {
-                        'u'
-                    }
-                },
-
-                // otherwise -> \
-                _ => '\\'
-            }
-        } else {
-            slice.char_at(0)
-        }];
-        i += 1;
-
-        slice = inp.slice(i, inp.len() - 1);
-    }
-
-    str::from_chars(out)
-}
-
-fn gen_lit(l: ast::Lit) -> types::Val<Interp> {
-    match l {
-        ast::IntegerLiteral(i) => types::Integer(int::from_str(i).unwrap()),
-        ast::FloatingLiteral(i) => types::Floating(float::from_str(i).unwrap()),
-        ast::StringLiteral(s) => types::String(unescape_str(s).to_managed()),
-        ast::BytesLiteral(s) => types::Bytes(at_vec::from_owned(unescape_str(s).to_bytes()))
-    }
-}
-
 fn eval_exp(interp: @mut Interp, env: @mut env::Env<Interp>, exp: &ast::Exp) -> types::Val<Interp> {
     let frame = interp.current_frame.unwrap();
 
     let r = match exp.exp {
         ast::UnitExpression => types::Unit,
-        ast::LiteralExpression(l) => gen_lit(l),
+        ast::LiteralExpression(l) => types::gen_lit(l),
         ast::LambdaExpression(pat, exp) => types::Function(@[types::Fun {
             pattern: pat,
             body: exp,
--- interp/types.rs
+++ interp/types.rs
@@ -64,3 +64,90 @@ impl <Interp> Eq for Val<Interp> {
         !self.eq(other)
     }
 }
+
+pub fn unescape_str(inp: &str) -> ~str {
+    let mut out = ~[];
+    let mut i = 1;
+    let mut slice = inp.slice(1, inp.len() - 1);
+
+    while slice.len() > 0 {
+        out += ~[if slice.char_at(0) == '\\' {
+            match slice.char_at(1) {
+                // \" -> "
+                '"' => {
+                    i += 1;
+                    '"'
+                },
+
+                // \' -> '
+                '\'' => {
+                    i += 1;
+                    '\''
+                },
+
+                // \\ -> \
+                '\\' => {
+                    i += 1;
+                    '\\'
+                },
+
+                // \n -> <LF>
+                'n' => {
+                    i += 1;
+                    '\n'
+                },
+
+                // \r -> <CR>
+                'r' => {
+                    i += 1;
+                    '\r'
+                },
+
+                // \t -> <TAB>
+                't' => {
+                    i += 1;
+                    '\t'
+                },
+
+                // \x## -> hex escape
+                'x' => {
+                    if slice.len() >= 4 {
+                        i += 3;
+                        int::from_str_radix(slice.slice(2, 4), 16).unwrap() as char
+                    } else {
+                        'x'
+                    }
+                },
+
+                // \u#### -> unicode escape
+                'u' => {
+                    if slice.len() >= 6 {
+                        i += 5;
+                        int::from_str_radix(slice.slice(2, 6), 16).unwrap() as char
+                    } else {
+                        'u'
+                    }
+                },
+
+                // otherwise -> \
+                _ => '\\'
+            }
+        } else {
+            slice.char_at(0)
+        }];
+        i += 1;
+
+        slice = inp.slice(i, inp.len() - 1);
+    }
+
+    str::from_chars(out)
+}
+
+pub fn gen_lit<Interp>(l: ast::Lit) -> Val<Interp> {
+    match l {
+        ast::IntegerLiteral(i) => Integer(int::from_str(i).unwrap()),
+        ast::FloatingLiteral(i) => Floating(float::from_str(i).unwrap()),
+        ast::StringLiteral(s) => String(unescape_str(s).to_managed()),
+        ast::BytesLiteral(s) => Bytes(at_vec::from_owned(unescape_str(s).to_bytes()))
+    }
+}
